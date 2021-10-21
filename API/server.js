942f7dcdcf4f4ff44c0c1ff2a804ca3d12b32993
liquidrinu <liquidrinu@gmail.com>
2021-10-21 11:49:01 +0200

    first push

---
+++ API/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const router = require('./router.js');
+
+const hostname = 'localhost';
+const port = 9999;
+
+app.use('/', router);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
