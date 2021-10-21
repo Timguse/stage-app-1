67cf8abab28203cda7bb6349d8c39177a3c221db
Timguse <tguse@casema.nl>
2021-10-21 12:25:05 +0200

    added env and base services

---
+++ src/services/base/index.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import env from "react-dotenv";
+
+class AuthorizedService {
+  constructor() {
+    this.http = axios.create({
+      baseURL,
+    });
+    this.public = axios.create({
+      baseURL,
+    });
+    this.http.interceptors.request.use((config) => {
+      const newConfig = { ...config };
+      const token = JSON.parse(window.localStorage.getItem("token"));
+      newConfig.headers.Authorization = `Bearer ${token}`;
+      return newConfig;
+    });
+    this.http.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (
+          error?.response?.status === 401 &&
+          error?.response?.data.message?.includes("Unauthenticated")
+        ) {
+          window.localStorage.setItem("token", null);
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+}
+export default AuthorizedService;
